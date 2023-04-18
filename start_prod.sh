7c7b47023c7bfe70b485faebb29784a635d130e9
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-04-18 23:50:11 +0500

    CHANGE: changed port

--- start_prod.sh
+++ start_prod.sh
@@ -1,4 +1,4 @@
 python manage.py makemigrations --noinput
 python manage.py collectstatic --noinput
 python manage.py migrate --run -syncdb
-gunicorn --workers=2 --worker-class=gthread --log-level debug --timeout=30 --threads=4 server.wsgi -b 0.0.0.0:8000 
+gunicorn --workers=2 --worker-class=gthread --log-level debug --timeout=30 --threads=4 server.wsgi -b 0.0.0.0:8001 
