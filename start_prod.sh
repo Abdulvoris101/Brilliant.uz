41478fea38687b4b25867750e9ce282b6507ffa5
Abdulvoris101 <erkinovabdulvoris101@gmail.com>
2023-08-08 21:06:30 +0500

    ADD: added shell

--- start_prod.sh
+++ start_prod.sh
@@ -2,4 +2,4 @@ python manage.py makemigrations --noinput
 python manage.py collectstatic --noinput
 python manage.py migrate
 nohup python manage.py run &
-gunicorn --workers=1 --worker-class=gthread --log-level error --timeout=30 --threads=4 server.wsgi -b 0.0.0.0:8001 
+uwsgi --ini uwsgi.ini
